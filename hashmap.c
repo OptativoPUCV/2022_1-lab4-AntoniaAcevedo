0cb976091050b2a36a18f4776b6de63a0ac8e9c8
AntoniaAcevedo1 <>
2022-04-10 22:09:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -116,6 +116,14 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
+  for (int i = map -> current+1; i < map->capacity; i++){
+    if (map -> buckets[i] != NULL ){
+      if (map->buckets[i]->key != NULL){
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
+  }
   return NULL;
 
 }
